8be775cc748314d83d9e44839b89ffb089559f11
xiaoma521 <1273114582@qq.com>
2021-03-05 13:03:11 -0500

    assignment
    update

---
+++ public/script.js
@@ -0,0 +1,29 @@
+async function windowActions(){
+    console.log('window loaded');
+    const form = document.querySelector('wrapper');
+    const search = document.querySelector('search');
+
+    const request = await fetch('/api')
+    const data = await request.jason();
+
+
+    form.addEventListener('submit', async(event) =>{
+     event.preventDefault();
+     console.log('submit fired', search.value);
+     const display = data.filter((record) => record.search.toUpperCase() === search.value.toUpperCase());
+
+});
+
+    search.addEventListener('input',(event)=>{
+
+        console.log('input', event.target.value);
+
+
+
+
+
+    });
+
+}
+   
+window.onload = windowActions
