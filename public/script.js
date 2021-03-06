db7e2a0efa36911691ca95c34b55258bbc48db18
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-03-05 20:36:50 -0500

    Added: Comments for anyone who is reading our code.

--- public/script.js
+++ public/script.js
@@ -7,13 +7,14 @@ async function windowActions(){
     const request = await fetch('/api');
     const data = await request.json();
     
-
+    // This code fires when the app is submitted
+    // The data list is then filtered and then returned to the HTML
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
         console.log('submit fired', search.value);
         const filtered = data.filter((record) => record.search.toUpperCase() === search.value.toUpperCase());
     });
-
+    // This section listens to any typing going on in the input box
     search.addEventListener('input', (event) => {
         console.log('input', event.target.value);
     })
