873c2fc843edcd82c4ea93ab010e0186bfd2448a
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-03-05 20:06:51 -0500

    Update: Moved const filtered under form.addeventlistener

--- public/script.js
+++ public/script.js
@@ -5,11 +5,12 @@ async function windowActions(){
 
     const request = await fetch('/api');
     const data = await request.json();
-    const filtered = data.filter((record) => record.search.toUpperCase() === search.value.toUpperCase());
+    
 
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
         console.log('submit fired', search.value);
+        const filtered = data.filter((record) => record.search.toUpperCase() === search.value.toUpperCase());
     });
 
     search.addEventListener('input', (event) => {
